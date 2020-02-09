204d9b9b34ffd968b7662e7ad2074be0dd6a4502
Haolin Zhang <frexiona@foxmail.com>
2020-02-09 13:51:30 +0000

    knowledge graph wrapper

--- knowledge_graph_wrapper.py
+++ knowledge_graph_wrapper.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 08/02/2020
+# @Author  : Haolin Zhang
+# @File    : knowledge_graph_wrapper.py
+# @Software: VS Code
+
 import google_search as search
-import ./wiki_model/pretrained_model as word2vec
+import wiki_model.pretrained_model as pretrained_word2vec
 
 if __name__ == '__main__':
     pass
